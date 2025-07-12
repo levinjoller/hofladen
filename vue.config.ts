32ca4a9420c9ef7f5e83d6f7983b357829f388d8
Levin Joller <45421206+levinjoller@users.noreply.github.com>
2025-07-12 12:53:40 +0200

    set base url correctly

--- vue.config.ts
+++ vue.config.ts
@@ -1,11 +1,7 @@
-import { ProjectOptions } from '@vue/cli-service';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-const REPO_NAME = 'hofladen';
-
-const config: ProjectOptions = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? (process.env.VUE_APP_BRANCH === 'dev' ? `/${REPO_NAME}/dev/` : `/${REPO_NAME}/`)
-        : '/', // <-- Dies ist für die lokale Entwicklung (NODE_ENV ist 'development')
-};
-
-export default config;
+export default defineConfig({
+    plugins: [vue()],
+    base: '/hofladen/'
+})
