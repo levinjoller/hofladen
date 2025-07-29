169009862ddda2f9dbc51035f6eda8fa913a55ab
levinjoller <levinjoller@users.noreply.github.com>
2025-07-29 14:07:09 +0000

    deploy: 712d916d98c88c6adcdf33f11756ab5f47f3bf3f

---
+++ assets/status-tap-Bi0FlRpD.js
@@ -0,0 +1,3 @@
+import{X as a,Y as i,Z as c,$ as d,a0 as l}from"./index-DDtXSPdP.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
