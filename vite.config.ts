6607cfd6d9d40b9e718da6b3de46616d8244e63f
Levin Joller <45421206+levinjoller@users.noreply.github.com>
2025-08-05 17:10:35 +0200

    update dependencies, reduce chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,43 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  base: process.env.NODE_ENV === 'production'
-    ? (process.env.VUE_APP_BRANCH === 'dev' ? '/hofladen/dev/' : '/hofladen/')
-    : '/',
+  plugins: [vue(), legacy()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? process.env.VUE_APP_BRANCH === "dev"
+        ? "/hofladen/dev/"
+        : "/hofladen/"
+      : "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
+    environment: "jsdom",
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("html2canvas")) {
+            return "html2canvas";
+          }
+          if (id.includes("@ionic/vue")) {
+            return "ionic";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
