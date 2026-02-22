e8a4b50088e5edb336e1aefc7eba1689f78e292f
Levin Joller <45421206+levinjoller@users.noreply.github.com>
2026-02-22 18:26:38 +0100

    add input password toggle for login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -6,40 +6,45 @@
       </ion-toolbar>
     </ion-header>
     <ion-content class="ion-padding">
-      <form @submit.prevent="handleLogin">
-        <ion-item>
-          <ion-input
-            label="E-Mail"
-            label-placement="stacked"
-            type="email"
-            v-model="email"
-            name="email"
-            autocomplete="username"
-            inputmode="email"
-            required
-          ></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-input
-            label="Passwort"
-            label-placement="stacked"
-            type="password"
-            v-model="password"
-            name="password"
-            autocomplete="current-password"
-            inputmode="text"
-            required
-          ></ion-input>
-        </ion-item>
-        <ion-button
-          type="submit"
-          expand="block"
-          :disabled="!isFormFilledOut || isLoading"
-        >
-          <span v-if="!isLoading">Login</span>
-          <ion-spinner v-else name="crescent"></ion-spinner>
-        </ion-button>
-      </form>
+      <div class="login-container">
+        <div class="login-header">
+          <h2>Willkommen zurück</h2>
+          <p>Bitte melden Sie sich an, um fortzufahren.</p>
+        </div>
+        <form @submit.prevent="handleLogin">
+          <ion-item>
+            <ion-input
+              label="E-Mail"
+              label-placement="stacked"
+              type="email"
+              v-model="email"
+              name="email"
+              autocomplete="username"
+              required
+            ></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input
+              label="Passwort"
+              label-placement="stacked"
+              type="password"
+              v-model="password"
+              name="password"
+              autocomplete="current-password"
+              required
+              ><ion-input-password-toggle slot="end"></ion-input-password-toggle
+            ></ion-input>
+          </ion-item>
+          <ion-button
+            type="submit"
+            expand="block"
+            :disabled="!isFormFilledOut || isLoading"
+          >
+            <span v-if="!isLoading">Login</span>
+            <ion-spinner v-else name="crescent"></ion-spinner>
+          </ion-button>
+        </form>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -56,6 +61,7 @@ import {
   IonInput,
   IonButton,
   IonSpinner,
+  IonInputPasswordToggle,
 } from "@ionic/vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth-store";
@@ -81,3 +87,23 @@ const handleLogin = async () => {
   }
 };
 </script>
+<style scoped>
+.login-container {
+  max-width: 450px;
+  margin: 40px auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.login-header {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+@media (max-width: 480px) {
+  .login-container {
+    margin-top: 20px;
+    max-width: 100%;
+  }
+}
+</style>
